2bcf975ca28667402ad6d8ae3481c3a714cb62f0
pep Farin <pepfarin@gmail.com>
2017-09-20 16:58:40 +0200

    New Header & Footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,17 @@
 
+// Disabled Links
+var x = document.querySelectorAll('a.disabled');
+var i = 0;
+for( i = 0; i < x.length; i++) {
+    x[i].addEventListener('click', function(e){
+        e.preventDefault();
+    })
+};
 
-// SelectText
-function SelectText(element) {
-  var doc = document,
-      text = element,
-      range, selection;
-
-  if (doc.body.createTextRange) {
-      range = document.body.createTextRange();
-      range.moveToElementText(text);
-      range.select();
-      /*
-      if(range.toString().length === 0){
-        range.moveToElementText(text);
-        range.select();
-      }
-      */
-  } else if (window.getSelection) {
-      selection = window.getSelection();
-      if(selection.toString().length === 0){
-        range = document.createRange();
-        range.selectNodeContents(text);
-        selection.removeAllRanges();
-        selection.addRange(range);
-      }
-  }
-}
+var x = document.querySelectorAll('a.home-disabled');
+var i = 0;
+for( i = 0; i < x.length; i++) {
+    x[i].addEventListener('click', function(e){
+        e.preventDefault();
+    })
+};
