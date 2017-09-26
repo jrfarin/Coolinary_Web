f96f4c9fd3a30dc260641c9b14a7f0f08700381a
pep Farin <pepfarin@gmail.com>
2017-09-26 18:22:00 +0200

    Recetas Octubre 4,5,6, Cambios Generales

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,11 @@ for( i = 0; i < x.length; i++) {
         e.preventDefault();
     })
 };
+
+var x = document.querySelectorAll('a.recetas-disabled');
+var i = 0;
+for( i = 0; i < x.length; i++) {
+    x[i].addEventListener('click', function(e){
+        e.preventDefault();
+    })
+};
